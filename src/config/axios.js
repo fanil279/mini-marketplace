4e57c7f66bd554d18c5dbf0db2e7dc11fe0db642
fanil279 <fanilramakaev@gmail.com>
2025-12-15 16:51:35 +0500

    feat: enhance layout and styling of products and cart sections, update axios timeout

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    timeout: 10000,
+    timeout: 3000,
     headers: {
         'Content-Type': 'application/json',
     },
